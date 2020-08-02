ca19be69c8e3985cf3600bfc615f7228b5e1ab44
1994reka3 <rik3@re1kas-MacBookAir.local>
2020-08-02 17:06:36 +0900

    [Add]Book投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,12 @@
 class BooksController < ApplicationController
 
+
+
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice]='You have created book successfully.'
-      redirect_to book_path
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
     else
       @books = Book.all
       render :index
@@ -14,19 +16,30 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
-
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
