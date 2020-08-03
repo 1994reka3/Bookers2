20d1665bfc246d7123f233d7f8f8d127814dc2bd
1994reka3 <rik.s.1005@gmail.com>
2020-08-04 01:39:58 +0900

    [Update]User_introduction バリデーション

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '371e42ec605ea98c04a89d63f393cf5a8baa33e175a37b2de940496f412041fafb59589144ecbeea51f131487f3e4561be2d3e4a263afc7e60ac484db92e1aaf'
