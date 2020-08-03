6b76fa158650abdc8d7a1e2e41771c89ab8f5d86
1994reka3 <rik.s.1005@gmail.com>
2020-08-03 19:45:02 +0900

    [Add]部分テンプレート作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
   end
 
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_114628) do
+ActiveRecord::Schema.define(version: 2020_08_03_080644) do
 
   create_table "books", force: :cascade do |t|
     t.integer "user_id"
     t.text "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_images", force: :cascade do |t|
+    t.string "image_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -29,6 +36,8 @@ ActiveRecord::Schema.define(version: 2020_08_01_114628) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "introduction"
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
