cb86f8ae9aa1eb6071eca7456df5c9550da5c57e
1994reka3 <rik.s.1005@gmail.com>
2020-08-04 16:12:20 +0900

    [Add]ヘッダー完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
+  get 'homes/about' => 'homes#about'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
+
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
 
 end
