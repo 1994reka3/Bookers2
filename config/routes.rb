26452335869cd372e56ba5ee5db40b4e27276f15
vagrant <vagrant@localhost.localdomain>
2020-08-17 14:09:13 +0000

    [Add]いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
 
 end
